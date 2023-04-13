fe994d3e0fd9bc5b64b523bbb3dfc524726b35bd
engendromestre <engendro.mestre@gmail.com>
2023-04-13 11:17:41 -0300

    remove middleware verified

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
-})->middleware(['auth','verified'])->name('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
