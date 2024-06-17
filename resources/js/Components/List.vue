3dce598ef81375b61db624af6d1458ff38422228
engendromestre <engendro.mestre@gmail.com>
2024-06-17 21:38:44 +0000

    export to pdf

--- resources/js/Components/List.vue
+++ resources/js/Components/List.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { ref, watch } from "vue";
 import { Inertia } from "@inertiajs/inertia";
-import { PlusIcon } from "@heroicons/vue/24/outline";
+import { PlusIcon, DocumentArrowDownIcon } from "@heroicons/vue/24/outline";
 import Paginate from "@/Components/Paginate.vue";
 import Table from "@/Components/Table.vue";
+import TableToPDF from "@/Components/TableToPDF.vue";
 import AlertWarning from "@/Components/AlertWarning.vue";
 import { useStore } from "vuex";
+import html2pdf from "html2pdf.js";
 
 const props = defineProps({
     path: String,
@@ -51,9 +53,30 @@ watch([q], ([query]) => {
 });
 
 const dataLength = props.data.data.length;
+
+
+function exportToPDF() {
+    const element = document.getElementById('printableArea');
+    const options = {
+        margin: 1,
+        filename: 'meu-documento.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+    };
+
+    // Temporariamente tornar a div visível para capturar o conteúdo
+    element.style.visibility = 'visible';
+
+    // Use html2pdf para gerar o PDF
+    html2pdf().from(element).set(options).save().finally(() => {
+        // Esconder a div novamente após a captura do conteúdo
+        element.style.visibility = 'hidden';
+    });
+}
 </script>
 <template>
-    <div class="py-12">
+    <div class="py-12" ref="document">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-5">
             <div class="overflow-hidden">
                 <div class="flex justify-end items=center p-5">
@@ -62,8 +85,8 @@ const dataLength = props.data.data.length;
                             <input type="text" id="floating_standard"
                                 class="block py-2.5 px-0 w-full text-sm text-gray-900
                                 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white
-                                dark:border-gray-600 dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:border-gray-600 peer"
-                                placeholder=" " v-model="q" autocomplete="off" />
+                                dark:border-gray-600 dark:focus:border-gray-500 focus:outline-none focus:ring-0 focus:border-gray-600 peer" placeholder=" "
+                                v-model="q" autocomplete="off" />
                             <label for="floating_standard"
                                 class="absolute text-sm text-gray-500 dark:text-gray-300 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-gray-600 peer-focus:dark:text-gray-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                 {{ translate("Find") }}
@@ -75,13 +98,21 @@ const dataLength = props.data.data.length;
                             <span class="iconify mr-1" data-icon="gridicons:create" data-inline="false"></span>
                             <PlusIcon class="h-6 w-6 text-green-700" aria-hidden="true" />
                         </button>
+                        <button type="button" :title="translate('Export to PDF')" @click="exportToPDF"
+                            class="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2">
+                            <span class="iconify mr-1" data-icon="gridicons:create" data-inline="false"></span>
+                            <DocumentArrowDownIcon class="h-6 w-6 text-red-700" aria-hidden="true" />
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-2">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" v-if="dataLength > 0">
+            <div class="overflow-hidden shadow-sm sm:rounded-lg" v-if="dataLength > 0">
                 <Table :fields="fields" :items="data" :can="can" :lang="lang"></Table>
+                <div id="printableArea">
+                    <TableToPDF :fields="fields" :items="data" :can="can" :lang="lang" />
+                </div>
             </div>
             <div class="p-16 bg-white h-48 overflow-hidden shadow-sm sm:rounded-lg" v-else>
                 <AlertWarning :message="translate('No record found')" />
@@ -96,3 +127,11 @@ const dataLength = props.data.data.length;
         </div>
     </div>
 </template>
+
+<style scoped>
+#printableArea {
+  visibility: hidden; /* Esconde a div */
+  background: white;
+  padding: 20px;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
         './resources/js/**/*.vue',
+        './node_modules/flowbite/**/*.js',
     ],
 
     theme: {
@@ -18,7 +19,8 @@ module.exports = {
         },
     },
     plugins: [
-        require('@tailwindcss/forms')
+        require('@tailwindcss/forms'),
+        require('flowbite/plugin')
     ],
 
 };
