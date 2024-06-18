5a59f0b864a56b337747422fcca39742762d94f1
engendromestre <engendro.mestre@gmail.com>
2024-06-18 17:50:25 +0000

    finished route socialite

---
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class ProviderController extends Controller
+{
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    public function callback($provider)
+    {
+        try {
+            $socialUser = Socialite::driver($provider)->user();
+            if (User::where('email', $socialUser->getEmail())
+                ->whereNull('provider_id')
+                ->exists()
+            ) {
+                return redirect()->route('welcome')->withErrors(['email' => 'This email uses different method to login']);
+            }
+
+            $user = User::where([
+                'provider' => $provider,
+                'provider_id' => $socialUser->id
+
+            ])->first();
+
+            if (!$user) {
+                $user = User::create([
+                    'name' => $socialUser->getName(),
+                    'email' => $socialUser->getEmail(),
+                    'provider' => $provider,
+                    'provider_id' => $socialUser->getId(),
+                    'provider_token' => $socialUser->token,
+                ]);
+                $user->markEmailAsVerified();
+            }
+
+            Auth::login($user);
+            return redirect(RouteServiceProvider::HOME);
+        } catch (\Exception $e) {
+            return redirect()->route('login')->withErrors(['email' => 'This email uses different method to login']);
+        }
+    }
+}
