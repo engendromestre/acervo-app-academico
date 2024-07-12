2360bcd229c357b84bf864b7b21731f6c1f070d5
engendromestre <engendro.mestre@gmail.com>
2024-07-12 14:46:53 -0300

    ci-cd main workflow adding test database

--- .github/actions/config-laravel/entrypoint.sh
+++ .github/actions/config-laravel/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-cp .env.example .env
+php -r "file_exists('.env') || copy('.env.example', '.env');"
 php artisan cache:clear
 php artisan config:clear
 php artisan key:generate
+chmod -R 777 storage bootstrap/cache
