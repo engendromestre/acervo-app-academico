f31ac6d3428f075d2195274cd1f4ed815fc7dc82
engendromestre <engendro.mestre@gmail.com>
2024-07-12 14:49:23 -0300

    ci-cd main workflow adding test database with migrate seeds

--- .github/actions/config-laravel/entrypoint.sh
+++ .github/actions/config-laravel/entrypoint.sh
@@ -3,4 +3,6 @@ php -r "file_exists('.env') || copy('.env.example', '.env');"
 php artisan cache:clear
 php artisan config:clear
 php artisan key:generate
+php artisan migrate --seed
+php artisan storage:link
 chmod -R 777 storage bootstrap/cache
