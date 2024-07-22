054b56c0f8108b942b13b4f2b12a9786103c5495
engendromestre <engendro.mestre@gmail.com>
2024-07-22 17:43:39 -0300

    github actions azure-deploy-new 4

--- .github/actions/azure-deploy/startup.sh
+++ .github/actions/azure-deploy/startup.sh
@@ -1,3 +1,7 @@
+# copy configuration files
+cp /home/default.conf /etc/nginx/sites-enabled/default.conf
+cp /home/php.ini /usr/local/etc/php/conf.d/php.ini
+
 # install support for webp file conversion
 apt-get update --allow-releaseinfo-change && apt-get install -y libfreetype6-dev \
                 libjpeg62-turbo-dev \
@@ -8,3 +12,39 @@ docker-php-ext-install gd
 
 # install support for queue
 apt-get install -y supervisor
+
+cp /home/laravel-worker.conf /etc/supervisor/conf.d/laravel-worker.conf
+
+# restart nginx
+service nginx restart
+service supervisor restart
+
+php /home/site/wwwroot/artisan down --refresh=15 --secret="1630542a-246b-4b66-afa1-dd72a4c43515"
+
+# Clear caches
+php /home/site/wwwroot/artisan cache:clear
+
+# Clear and cache routes
+php /home/site/wwwroot/artisan route:cache
+
+# Clear and cache config
+php /home/site/wwwroot/artisan config:cache
+
+# Clear and cache views
+php /home/site/wwwroot/artisan view:cache
+
+# Install node modules
+npm ci
+
+# Compile npm assets with build
+npm run build
+
+# uncomment next line if you dont have S3 or Blob storage
+php /home/site/wwwroot/artisan storage:link
+
+# Turn off maintenance mode
+php /home/site/wwwroot/artisan up
+
+# run worker
+nohup php /home/site/wwwroot/artisan queue:work &
+
