ca8c9a61fb518439d9c10b02245fc79cf03ac1db
engendromestre <engendro.mestre@gmail.com>
2024-07-24 14:19:20 -0300

    github actions azure-deploy-new https assets

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig(({ mode }) => {
-    const isDevelopment = mode === 'development';
+    const isDevelopment = mode === process.env.VITE_ENV === 'development';
 
     return {
-        server: { 
+        server: {
+            https: isDevelopment ? false : true, // Usar HTTPS se não estiver em desenvolvimento
             hmr: {
                 host: 'localhost',
                 protocol: isDevelopment ? 'http' : 'https',
