e49179c772812a5c4bf30794591d59600393e56e
engendromestre <engendro.mestre@gmail.com>
2024-07-24 14:04:32 -0300

    github actions azure-deploy-new https assets

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,29 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-    server: { //local env only
-        hmr: {
-            host: 'localhost',
-        },
-    },
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
+export default defineConfig(({ mode }) => {
+    const isDevelopment = mode === 'development';
+
+    return {
+        server: { 
+            hmr: {
+                host: 'localhost',
+                protocol: isDevelopment ? 'http' : 'https',
             },
-        }),
-    ],
+        },
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+    };
 });
