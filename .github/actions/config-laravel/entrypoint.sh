2309166780cfdc050c42c7b144321e42a6a35aa7
engendromestre <engendro.mestre@gmail.com>
2024-07-25 16:30:46 -0300

    change public storage to s3 public storage

--- .github/actions/config-laravel/entrypoint.sh
+++ .github/actions/config-laravel/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
+chmod -R 777 storage bootstrap/cache
 php -r "file_exists('.env') || copy('.env.example', '.env');"
 php artisan cache:clear
 php artisan config:clear
 php artisan key:generate
 php artisan storage:link
-chmod -R 777 storage bootstrap/cache
